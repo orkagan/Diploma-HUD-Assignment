6bcc6f90acc5b60c5f9087497c698adc139eae21
Jonathan Wang <arachnophobicspyder@gmail.com>
2023-03-01 20:28:29 +1100

    Project Created
    Just put the basic first person controller that we did in the cert IV into the sample scene.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed = 5f;
+    public float gravity = 20f;
+        
+    Vector3 moveDir;
+    private CharacterController _charCtrl;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _charCtrl = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_charCtrl.isGrounded)
+        {
+            moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDir.y += jumpSpeed;
+            }
+        }
+        moveDir.y -= gravity * Time.deltaTime;
+        _charCtrl.Move(transform.TransformDirection(moveDir) * speed * Time.deltaTime);
+    }
+}
